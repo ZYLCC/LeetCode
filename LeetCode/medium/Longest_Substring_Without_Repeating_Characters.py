07fc49f9a77ee10aad752b6d545364e5b2b74bfc
lcc <15334517687@163.com>
2022-03-11 20:54:53 +0800

    add new file

--- LeetCode/medium/Longest_Substring_Without_Repeating_Characters.py
+++ LeetCode/medium/Longest_Substring_Without_Repeating_Characters.py
@@ -2,18 +2,39 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s) -> int:
-        #
-        tmp = []
-        maxlength = 0
-        for each in s:
-            if each not in tmp:
-                tmp.append(each)
-                if len(tmp) > maxlength:
-                    maxlength = len(tmp)
-            else:
-                tmp = tmp[tmp.index(each)+1:]
-                tmp.append(each)
-        return maxlength
+        # tmp = []
+        # maxlength = 0
+        # for each in s:
+        #     if each not in tmp:
+        #         tmp.append(each)  # 将未出现的按顺序写入tmp
+        #         if len(tmp) > maxlength:
+        #             maxlength = len(tmp)
+        #     else:  # 将和each重复的字符及左边的所有字符删除，并将each写入tmp最右边
+        #         tmp = tmp[tmp.index(each) + 1:]
+        #         tmp.append(each)
+        # return maxlength
+
+
+        # 官方题解，思想类似
+        # 哈希集合，记录每个字符是否出现过
+        occ = set()
+        n = len(s)
+        # 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        rk, ans = -1, 0
+        for i in range(n):
+            if i != 0:
+                # 左指针向右移动一格，移除一个字符
+                occ.remove(s[i - 1])
+            while rk + 1 < n and s[rk + 1] not in occ:
+                # 不断地移动右指针
+                occ.add(s[rk + 1])
+                rk += 1
+            # 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = max(ans, rk - i + 1)
+        return ans
+
+
+
 
 
 
---
+++ LeetCode/simple/Add_Binary.py
@@ -0,0 +1,36 @@
+class Solution:
+    def addBinary(self, a, b) -> str:
+        # 内置函数
+        # return bin(int(a, 2) + int(b, 2))[2:]  # 返回的二进制是“0b....”，所以分片[2:]
+
+        # 模拟二进制求和
+        n = max(len(a), len(b))
+        a = [0] * (n - len(a) + 1) + list(map(int, a)) # 让a,b位数补齐，且前面均预留一位用于进位
+        b = [0] * (n - len(b) + 1) + list(map(int, b))
+        plus = [0 for _ in range(n + 1)]
+        # plus = [a[i] + b[i] for i in range(n + 1)]
+        for i in range(n, 0, -1):
+            c = a[i] + b[i]
+            if c + plus[i] <= 1:
+                plus[i] += c
+            elif c + plus[i] == 2:
+                plus[i] = 0
+                plus[i - 1] = 1
+            elif c + plus[i] == 3:
+                plus[i] = 1
+                plus[i - 1] = 1
+
+        re = ''.join(list(map(str, plus)))
+        return re if plus[0] == 1 else re[1:]
+
+test = Solution()
+print(test.addBinary('11', '1'))
+print(test.addBinary('1010', '1011'))
+
+
+
+
+
+
+
+
